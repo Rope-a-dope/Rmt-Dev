7d88ffec6fe6979c4b1b3e81f1b7c29edb6ba564
Aaron Zhang <ropeadope2021@gmail.com>
2024-06-04 17:36:57 -0700

    final code for project rmtdev

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,52 @@
+import { Toaster } from "react-hot-toast";
+import Background from "./Background";
+import BookmarksButton from "./BookmarksButton";
+import Container from "./Container";
+import Footer from "./Footer";
+import Header, { HeaderTop } from "./Header";
+import JobItemContent from "./JobItemContent";
+import JobListSearch from "./JobListSearch";
+import Logo from "./Logo";
+import PaginationControls from "./PaginationControls";
+import ResultsCount from "./ResultsCount";
+import SearchForm from "./SearchForm";
+import Sidebar, { SidebarTop } from "./Sidebar";
+import SortingControls from "./SortingControls";
+
 function App() {
-  return <>Hello!</>;
+
+  return (
+    <>
+      <Background />
+
+      <Header>
+        <HeaderTop>
+          <Logo />
+          <BookmarksButton />
+        </HeaderTop>
+
+        <SearchForm />
+      </Header>
+
+      <Container>
+        <Sidebar>
+          <SidebarTop>
+            <ResultsCount />
+            <SortingControls />
+          </SidebarTop>
+
+          <JobListSearch />
+
+          <PaginationControls />
+        </Sidebar>
+        <JobItemContent />
+      </Container>
+
+      <Footer />
+
+      <Toaster position="top-right" />
+    </>
+  );
 }
 
 export default App;
